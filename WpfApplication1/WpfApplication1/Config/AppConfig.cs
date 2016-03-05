c7cc2c38946bc99a09660fe9147fd1514c01ca22
Jingguo Feng <everjgfeng@gmail.com>
2016-03-04 21:37:59 -0600

    first commit

---
+++ WpfApplication1/WpfApplication1/Config/AppConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1.Config
+{
+    public class AppConfig
+    {
+        public AppConfig()
+        {
+
+        }
+
+        public void ReadConfig(string s)
+        {
+            StreamReader reader = new StreamReader(s);
+
+
+        }
+    }
+}
---
+++ WpfApplication1/WpfApplication1/Lib/BaseHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1.Lib
+{
+    public class BaseHelper
+    {
+        public BaseHelper()
+        {
+
+        }
+
+        public void Start()
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
